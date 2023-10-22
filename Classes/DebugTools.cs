b67c820c0ef21d2048feb95f6924f8d0cc2865e3
Aren Julhakyan <julhakyan.rn@gmail.com>
2023-10-22 15:16:10 +0400

    Alpha 0.3.9.2

--- Classes/DebugTools.cs
+++ Classes/DebugTools.cs
@@ -9,7 +9,7 @@ namespace Battleship
     public static class DebugTools
     {
         public static bool DebugMode = true;
-        public static string Version = "Alpha 0.3.9.1";
+        public static string Version = "Alpha 0.3.9.2";
 
         public static class MCF
         {
