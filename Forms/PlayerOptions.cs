085f21e373e7b526d36bdb3d64af4729443675f1
Aren Julhakyan <mgc.julhakyan@gmail.com>
2024-05-03 17:10:05 +0400

    Pre-Release 1.3.1.0

--- Forms/PlayerOptions.cs
+++ Forms/PlayerOptions.cs
@@ -17,11 +17,6 @@ namespace Battleship.Forms
         {
             InitializeComponent();
         }
-        private void PlayerOptions_Load(object sender, EventArgs e)
-        {
-            Design.ChangeControlElementsForeColor(this, Design.DefaultForeColor, DefaultBackColor);
-            SetFormComponentsLocations();
-        }
         public void SetFormComponentsLocations()
         {
             Design.SetComponentLocation(L_Info_PlayerName, PNL_PO_Main);
@@ -30,14 +25,52 @@ namespace Battleship.Forms
             CHB_UseUsername.Location = new Point(TB_PlayerName.Location.X, CHB_UseUsername.Location.Y);
             TB_PlayerName.Text = $"{Environment.UserName}";
         }
+        private void PlayerOptions_Load(object sender, EventArgs e)
+        {
+            Design.ChangeControlElementsForeColor(this, Design.DefaultForeColor, DefaultBackColor);
+            SetFormComponentsLocations();
+        }
+        private void PlayerOptions_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Opacity == 1)
+            {
+                MenuForm mf = new MenuForm();
+                Design.FormSwitching(this, mf, 1, false, 8);
+            }
+        }
         private void TB_PlayerName_MouseHover(object sender, EventArgs e)
         {
             TextBox hover = sender as TextBox;
-            TT_PO.Show("Enter your name", hover);;
+            TT_PO.Show("Enter your name", hover);
+        }
+        private void CB_GameMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CHB_MoreInfo.Text = $"More info about \"{CB_GameMode.SelectedItem}\"";
+            Options.GameModeInt = CB_GameMode.SelectedIndex;
+            switch (CB_GameMode.SelectedIndex)
+            {
+                case 0:
+                    {
+                        
+                        L_GameModeText.Text = $"In the \"{CB_GameMode.SelectedItem}\" you will receive\r\ninformation about the chances of hitting.\r\nBy choosing a specific cell on the map.\r\nDesigned for the study of probability.\r\n";
+                        Options.GameMode = "Education";
+                        break;
+                    }
+                case 1:
+                    { 
+                        L_GameModeText.Text = $"In \"{CB_GameMode.SelectedItem}\" you will not receive\r\ninformation about the chances of hitting.\r\nDesigned for casual play.";
+                        Options.GameMode = "Classic";
+                        break;
+                    }
+            }
+        }
+        private void CHB_MoreInfo_CheckedChanged(object sender, EventArgs e)
+        {
+            L_GameModeText.Visible = CHB_MoreInfo.Checked;
         }
         public void SetOptions()
         {
-            if (Options.SingleplayerMode == true)
+            if (Options.SingleplayerMode)
             {
                 if (TB_PlayerName.Text == string.Empty)
                 {
@@ -48,44 +81,8 @@ namespace Battleship.Forms
                     Options.SP_PlayerName = TB_PlayerName.Text;
                 }
             }
-            GameForm GPO = new GameForm();
-            Design.FormSwitching(this, GPO, 1, false, 8);
-        }
-        private void BS_ApplyPlayerData_Click(object sender, EventArgs e)
-        {
-            if (TB_PlayerName.Text == Environment.UserName)
-            {
-                DialogResult = MessageBox.Show($"Start game with your computer username: {TB_PlayerName.Text}?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                switch (DialogResult)
-                {
-                    case DialogResult.Yes:
-                        {
-                            SetOptions();
-                            break;
-                        }
-                    case DialogResult.No:
-                        {
-                            TB_PlayerName.Clear();
-                            break;
-                        }
-                }
-            }
-            else if (TB_PlayerName.Text == string.Empty)
-            {
-                DialogResult = MessageBox.Show("Start game with default username?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (DialogResult == DialogResult.Yes)
-                {
-                    SetOptions();
-                }
-            }
-            else
-            {
-                DialogResult = MessageBox.Show("Start game?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (DialogResult == DialogResult.Yes)
-                {
-                    SetOptions();
-                }
-            }
+            GameForm gf = new GameForm();
+            Design.FormSwitching(this, gf, 1, false, 8);
         }
         private void CHB_UseUsername_CheckedChanged(object sender, EventArgs e)
         {
@@ -103,39 +100,33 @@ namespace Battleship.Forms
                     }
             }
         }
-        private void PlayerOptions_FormClosing(object sender, FormClosingEventArgs e)
+        private void BS_ApplyPlayerData_Click(object sender, EventArgs e)
         {
-            if (Opacity == 1)
+            if (TB_PlayerName.Text == Environment.UserName)
             {
-                MenuForm mf = new MenuForm();
-                Design.FormSwitching(this, mf, 1, false, 8);
+                if (MessageBox.Show($"Start game with your computer username: {TB_PlayerName.Text}?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    SetOptions();
+                }
+                else
+                {
+                    TB_PlayerName.Clear();
+                }
             }
-        }
-        private void CB_GameMode_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            switch (CB_GameMode.SelectedIndex)
+            else if (TB_PlayerName.Text == string.Empty)
             {
-                case 0:
-                    {
-                        CHB_MoreInfo.Text = $"More info about \"{CB_GameMode.SelectedItem}\"";
-                        L_GameModeText.Text = $"In the \"{CB_GameMode.SelectedItem}\" you will receive\r\ninformation about the chances of hitting.\r\nBy choosing a specific cell on the map.\r\nDesigned for the study of probability.\r\n";
-                        Options.GameModeInt = CB_GameMode.SelectedIndex;
-                        Options.GameMode = "Education";
-                        break;
-                    }
-                case 1:
-                    {
-                        CHB_MoreInfo.Text = $"More info about {CB_GameMode.SelectedItem}";
-                        L_GameModeText.Text =  $"In \"{CB_GameMode.SelectedItem}\" you will not receive\r\ninformation about the chances of hitting.\r\nDesigned for casual play.";
-                        Options.GameModeInt = CB_GameMode.SelectedIndex;
-                        Options.GameMode = "Classic";
-                        break;
-                    }
+                if (MessageBox.Show("Start game with default username?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    SetOptions();
+                }
             }
-        }
-        private void CHB_MoreInfo_CheckedChanged(object sender, EventArgs e)
-        {
-            L_GameModeText.Visible = CHB_MoreInfo.Checked;
-        }
+            else
+            {
+                if (MessageBox.Show("Start game?", "Battleship", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    SetOptions();
+                }
+            }
+        }                
     }
 }
